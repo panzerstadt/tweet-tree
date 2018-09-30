f4539c3beac8c58dc10442f5da142a114d3dea9e
panzerstadt <tliqun@gmail.com>
2018-09-30 13:52:28 +0900

    entity structure fix

--- docker-app/utils/google_api.py
+++ docker-app/utils/google_api.py
@@ -157,40 +157,25 @@ def analyze_entities_api(text=''):
     return list_of_entities
 
 
-def entity_sentiment_text(text):
-    """Detects entity sentiment in the provided text."""
-    client = language.LanguageServiceClient()
+def analyze_entities(text=""):
+    result = analyze_entities_api(text)
 
-    if isinstance(text, six.binary_type):
-        text = text.decode('utf-8')
+    output = []
+    for r in result:
+        for k, v in r.items():
 
-    document = types.Document(
-        content=text.encode('utf-8'),
-        type=enums.Document.Type.PLAIN_TEXT)
+            output.append({
+                "label": k,
+                "salience": v['entity salience'],
+                "type": v['entity type']
 
-    # Detect and send native Python encoding to receive correct word offsets.
-    encoding = enums.EncodingType.UTF32
-    if sys.maxunicode == 65535:
-        encoding = enums.EncodingType.UTF16
-
-    result = client.analyze_entity_sentiment(document, encoding)
-
-    for entity in result.entities:
-        print('Mentions: ')
-        print(u'Name: "{}"'.format(entity.name))
-        for mention in entity.mentions:
-            print(u'  Begin Offset : {}'.format(mention.text.begin_offset))
-            print(u'  Content : {}'.format(mention.text.content))
-            print(u'  Magnitude : {}'.format(mention.sentiment.magnitude))
-            print(u'  Sentiment : {}'.format(mention.sentiment.score))
-            print(u'  Type : {}'.format(mention.type))
-        print(u'Salience: {}'.format(entity.salience))
-        print(u'Sentiment: {}\n'.format(entity.sentiment))
+            })
+    return output
 
 
 def analyze_entities_and_sentiment(text=""):
     sentiment = analyze_sentiment(text)
-    entities = analyze_entities_api(text)
+    entities = analyze_entities(text)
 
     try:
         magnitude = sentiment.magnitude
